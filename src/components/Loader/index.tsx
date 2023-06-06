a99db250e837b9c4d5e0fd570a3ff3bbde880366
Johiny <johiny7@live.com>
2023-06-05 22:18:38 -0500

    loader funcionando

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,8 @@
+import laoderstyles from './Loader.module.css'
+const Loader = () => {
+  return (
+    <span className={laoderstyles.loader}></span>
+  )
+}
+
+export default Loader
--- src/components/MoreResults.tsx
+++ src/components/MoreResults.tsx
@@ -3,20 +3,30 @@ import { animalType } from "@/utils/types";
 import useIntersectionObserver from "@/utils/useIntersectionObserver";
 import useSWRInfinite from 'swr/infinite'
 import AnimalCard from "./AnimalCard";
+import { useState, useEffect } from "react";
+import Loader from '@/components/Loader'
 
-const PAGE_SIZE = 6
-
-const getKey = (pageIndex : number, previousPageData : any, pageSize : number) => {
-  if (previousPageData && !previousPageData.data.length) return null // reached the end
+const getKey = (pageIndex : number, previousPageData : any, setEndReached: Function) => {
+  if (previousPageData && !previousPageData.data.length){
+    setEndReached(true)
+    return null
+  } // reached the end
   return `http://localhost:3000/api/paises/mexico?page=${pageIndex + 1}`                    // SWR key
 }
 
 const MoreResults = () => {
   const fetcher = (...args) => fetch(...args).then(res => res.json())
-  const { data, size, setSize } = useSWRInfinite((...args) => getKey(...args, PAGE_SIZE), fetcher)
-  const refetch = () => setSize(size + 1)
+  const [endReached, setEndReached] = useState(false)
+  const { data, size, setSize, isValidating } = useSWRInfinite((...args) => getKey(...args, setEndReached), fetcher)
+  const refetch = () => {
+    if(!endReached){
+      setSize(size + 1)
+    }
+  }
   const {targetRef} = useIntersectionObserver({threshold: 0.5}, refetch)
-
+  useEffect(() => {
+    console.log(endReached)
+  },[endReached])
   return (
     <>
      { data ? data.map(pages => {
@@ -31,7 +41,9 @@ const MoreResults = () => {
       )
      }) : <></>
     }
-    <div onClick={() => refetch()} ref={targetRef}  className="w-screen h-5 bg-black"></div>
+    <div onClick={() => refetch()} ref={targetRef}  className="w-screen flex justify-center mb-5 pt-1">
+      {isValidating ? <Loader/> : <h2 className="text-2xl">No hay más resultados</h2>}
+    </div>
     </>
   )
 }
