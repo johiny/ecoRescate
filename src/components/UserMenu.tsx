62b9fd0c6eff6721b700ce1813f155c845aec4fa
Johiny <johiny7@live.com>
2023-06-07 20:13:28 -0500

    error de menu al cargar pagina arreglado

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -7,14 +7,14 @@ import { useSession } from "next-auth/react";
 import HelpedAnimals from "./HelpedAnimals";
 
 const UserMenu = () => {
-    const [menuOpen, setMenuOpen] = useState(false)
+    const [menuOpen, setMenuOpen] = useState('initial')
     const session = useSession()
   return (
     <>
-     <div onClick={() => setMenuOpen(true)} className={" absolute  w-16 h-16  top-28 right-0 flex flex-row justify-center p-2 items-center cursor-pointer"}>
+     <div onClick={() => setMenuOpen(true)} className={"absolute  w-16 h-16  top-28 right-0 flex flex-row justify-center p-2 items-center cursor-pointer"}>
         <Image src={session.data?.user?.image || user} width={100} height={100} alt="user photo" className="rounded-full border-2 border-ecoDarkGreen aspect-square w-full hover:rotate-12 transition-all"/>
     </div>
-    <div className={`h-screen w-96 bg-ecoWhite fixed top-0 right-[-1000px] flex flex-col p-2 items-center gap-3 ${menuOpen ? 'lateralMenuEntrance' : 'lateralMenuExit'}`}>
+    <div className={`h-screen w-96 bg-ecoWhite fixed top-0 right-[-1000px] flex flex-col p-2 items-center gap-3 ${menuOpen == 'initial' ? '' : menuOpen ? 'lateralMenuEntrance' : 'lateralMenuExit'}`}>
          <Image src={session.data?.user?.image || user} width={150} height={150} alt="user photo" className="rounded-full border-2 border-ecoDarkGreen aspect-square hover:rotate-12 transition-all"/>
          <Image src={close} width={35} height={35} alt="user photo" className="absolute top-0 right-0 cursor-pointer"
          onClick={() => setMenuOpen(false)}/>
