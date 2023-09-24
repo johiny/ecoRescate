0a72fbc503ab0cc242825eb355769ec0c555d20a
Johiny <johiny7@live.com>
2023-09-24 10:26:19 -0500

    revalidate comebackef

--- src/app/paises/[country]/page.tsx
+++ src/app/paises/[country]/page.tsx
@@ -4,7 +4,7 @@ import MoreResults from "@/components/MoreResults"
 const CountryPage = async ({params} : {params : {country : string}}) => {
   let animals : [] = []
   try{
-    const res = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/paises/${params.country}?page=0`,{cache: "no-cache"})
+    const res = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/paises/${params.country}?page=0`,{next: {revalidate: 60 * 60 * 6}})
     const {data}  = await res.json()
     if(data){
       animals = data
